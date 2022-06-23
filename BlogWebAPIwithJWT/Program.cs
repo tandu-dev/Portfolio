ea2c40294f450d7cc688ce6644c0641c8b27b30d
Scott Rasbach <development@scottrassbach.net>
2022-06-23 18:20:40 -0400

    add basic JWT authorization

--- BlogWebAPIwithJWT/Program.cs
+++ BlogWebAPIwithJWT/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 using BlogWithJWT;
 
 
@@ -7,7 +12,81 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BlogDb>(options => 
     options.UseSqlite("Data Source=JWTBlog.db"));
 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+    {
+        var securityScheme = new OpenApiSecurityScheme
+        {
+            Name = "JWT Authentication",
+            Description = "Enter JWT Bearer token **_only_**",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer", // must be lower case
+            BearerFormat = "JWT",
+            Reference = new OpenApiReference
+            {
+                Id = JwtBearerDefaults.AuthenticationScheme,
+                Type = ReferenceType.SecurityScheme
+            }
+        };
+    c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {securityScheme, new string[] { }}
+    });
+    //c.OperationFilter<SwaggerAuthorizeCheckOperationFilter>();
+});
+builder.Services.AddSingleton<TokenService>(new TokenService());
+builder.Services.AddSingleton<IUserRepositoryService>(new UserRepositoryService());
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+});
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.MapPost("/login",   [AllowAnonymous] async (
+    [FromBody] UserModel userModel,  TokenService tokenService, 
+    IUserRepositoryService userRepositoryService, HttpResponse response) => {
+    var userDto = userRepositoryService.GetUser(userModel);
+    if (userDto == null)
+    {
+        response.StatusCode = 401;
+        return;
+    }
+    var token = tokenService.BuildToken(
+        builder.Configuration["Jwt:Key"], builder.Configuration["Jwt:Issuer"],
+         builder.Configuration["Jwt:Audience"], userDto);
+        await response.WriteAsJsonAsync(new { token = token });
+        return;
+    })
+    .Produces(StatusCodes.Status200OK)
+    .WithName("Login").WithTags("Accounts");
+
+app.MapGet("/AuthorizedResource", (Func<string>)(
+[Authorize] () => "Action Succeeded")
+).Produces(StatusCodes.Status200OK)
+.WithName("Authorized").WithTags("Accounts").RequireAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 
---
+++ BlogWebAPIwithJWT/TokenService.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+public interface ITokenService
+{
+    string BuildToken(string key, string issuer,string audience, UserDto user);
+}
+public class TokenService : ITokenService
+{
+    private TimeSpan ExpiryDuration = new TimeSpan(0, 30, 0);
+    public string BuildToken(string key, string issuer,string audience,  UserDto user)
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.NameIdentifier,
+            Guid.NewGuid().ToString())
+        };
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+        var tokenDescriptor = new JwtSecurityToken(issuer, audience, claims,
+        expires: DateTime.Now.Add(ExpiryDuration), signingCredentials: credentials);
+        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+    }
+}
